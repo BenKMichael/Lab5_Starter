8c895326dbc2cf1b01c20d9989631d492f9ec89c
Benjamin Michael <benkmichael@gmail.com>
2026-05-03 18:11:48 -0700

    Added unit testing

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
+## Setup: Name
+
 Benjamin Michael
 
-
+## Explore part 3 : Questions
 
 1. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No. This is a robust functionality of the application that might be too complex for unit-testing. Unit-testing is typically reserved for easiliy isolatable functions/features within a project. Something like text-box formatting could be something that can be unit-tested within this functionality.
+
+2. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes. This is a simple feature that can be easiliy isolated and tested independently of other project featrues. Unit-testing would allow faster iterative development in this case.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,103 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+describe('isPhoneNumber', () => {
+
+  test('parenthesis around area code', () => {
+    expect(isPhoneNumber('(413) 231-2122')).toBe(true);
+  });
+
+  test('only dashes', () => {
+    expect(isPhoneNumber('413-231-2122')).toBe(true);
+  });
+
+  test('no dashes', () => {
+    expect(isPhoneNumber('413 231 2122')).toBe(false);
+  });
+
+  test('non-digit', () => {
+    expect(isPhoneNumber('413 -23I-2122')).toBe(false);
+  });
+
+});
+
+describe('isEmail', () => {
+
+  test('two char after .', () => {
+    expect(isEmail('boomshakalaka@okbro.co')).toBe(true);
+  });
+
+  test('all caps', () => {
+    expect(isEmail('HELLOWORLD@ME.ORG')).toBe(true);
+  });
+
+  test('no ID', () => {
+    expect(isEmail('@gmail.com')).toBe(false);
+  });
+
+  test('dot in ID', () => {
+    expect(isEmail('name.jeff@gmail.com')).toBe(false);
+  });
+
+});
+
+describe('isStrongPassword', () => {
+
+  test('length 15', () => {
+    expect(isStrongPassword('Hello1234567890')).toBe(true);
+  });
+
+  test('length 4', () => {
+    expect(isStrongPassword('Hello1234567890')).toBe(true);
+  });
+
+  test('length 16', () => {
+    expect(isStrongPassword('Super12345678901')).toBe(false);
+  });
+
+  test('length 2', () => {
+    expect(isStrongPassword('Hi')).toBe(false);
+  });
+
+});
+
+describe('isDate', () => {
+
+  test('2 digit month', () => {
+    expect(isDate('01/25/2023')).toBe(true);
+  });
+
+  test('1 digit month', () => {
+    expect(isDate('1/25/2023')).toBe(true);
+  });
+
+  test('2 digit year', () => {
+    expect(isDate('01/25/23')).toBe(false);
+  });
+
+  test('3 digit month', () => {
+    expect(isDate('001/25/2023')).toBe(false);
+  });
+
+});
+
+describe('isHexColor', () => {
+
+  test('3 digit', () => {
+    expect(isHexColor('f1f')).toBe(true);
+  });
+
+  test('6 digit', () => {
+    expect(isHexColor('f12fe4')).toBe(true);
+  });
+
+  test('7 digit', () => {
+    expect(isHexColor('f34ef21')).toBe(false);
+  });
+
+  test('g in hex', () => {
+    expect(isHexColor('f4efg1')).toBe(false);
+  });
+
+});
+
