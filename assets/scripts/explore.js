046d1393f26a357b8b0116e2f0b8c0762feea14d
Benjamin Michael <benkmichael@gmail.com>
2026-05-03 17:19:56 -0700

    Added part: Explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,35 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voices = speechSynthesis.getVoices();
+  const voiceSelect = document.getElementById('voice-select');
+  const textBox = document.getElementById('text-to-speak');
+  const talkButton = document.querySelector('#explore button');
+  const faceImg = document.querySelector('#explore img');
+
+  for (const voice of voices) {
+    const option = document.createElement('option');
+
+    option.textContent = voice.name;
+    option.value = voice.name;
+
+    voiceSelect.appendChild(option);
+  }
+
+  talkButton.addEventListener('click', function(event) {
+
+    const utterance = new SpeechSynthesisUtterance(textBox.value);
+    utterance.voice = voices.find(v => v.name === voiceSelect.value);
+    
+    speechSynthesis.speak(utterance);
+
+    utterance.addEventListener('start', function() {
+      faceImg.src = 'assets/images/smiling-open.png';
+    });
+
+    utterance.addEventListener('end', function() {
+      faceImg.src = 'assets/images/smiling.png';
+    });
+
+  });
 }
