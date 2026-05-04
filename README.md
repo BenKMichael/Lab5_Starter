0cab12df84573e9cf366bf08734c68174e73263d
Benjamin Michael <benkmichael@gmail.com>
2026-05-03 18:50:29 -0700

    Fixed TTS not populating on initial load

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
-## Setup: Name
+## Setup: Name/s
 
 Benjamin Michael
 
-## Explore part 3 : Questions
+## Explore Part 3 : Questions
 
 1. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
 
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,23 +3,27 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const voices = speechSynthesis.getVoices();
   const voiceSelect = document.getElementById('voice-select');
   const textBox = document.getElementById('text-to-speak');
   const talkButton = document.querySelector('#explore button');
   const faceImg = document.querySelector('#explore img');
 
-  for (const voice of voices) {
-    const option = document.createElement('option');
-
-    option.textContent = voice.name;
-    option.value = voice.name;
-
-    voiceSelect.appendChild(option);
+  function populateVoices() {
+    const voices = speechSynthesis.getVoices();
+    if (voices.length === 0) return;
+    for (const voice of voices) {
+      const option = document.createElement('option');
+      option.textContent = voice.name;
+      option.value = voice.name;
+      voiceSelect.appendChild(option);
+    }
   }
 
-  talkButton.addEventListener('click', function(event) {
+  speechSynthesis.addEventListener('voiceschanged', populateVoices);
+  populateVoices();
 
+  talkButton.addEventListener('click', function(event) {
+    const voices = speechSynthesis.getVoices();
     const utterance = new SpeechSynthesisUtterance(textBox.value);
     utterance.voice = voices.find(v => v.name === voiceSelect.value);
     
